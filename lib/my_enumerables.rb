cb1f6e28a010b0ab195b1c62a8e29dd3ee44000f
Enntek <abluecar@gmail.com>
2022-08-06 00:46:00 -0400

    Work on inject/reduce

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -34,7 +34,7 @@ module Enumerable
 
     case self
     when Array
-      self.my_each { |item| truthy_arr << item if block.call(item) }
+      self.my_each { |el| truthy_arr << el if block.call(el) }
       truthy_arr
     when Hash
       self.my_each { |k, v| truthy_hash[k] = v if block.call(k, v) }
@@ -43,29 +43,12 @@ module Enumerable
   end
 
   def my_all?(pattern = nil, &block)
-    self.my_each { |item| return false unless pattern === item } unless block_given?
+    self.my_each { |el| return false unless pattern === el } unless block_given?
 
-    self.my_each { |item| return false unless block.call(item) } unless pattern
+    self.my_each { |el| return false unless block.call(el) } unless pattern
 
     true
   end
-
-  # def my_tot?(pattern = nil)
-  # this line is interesting, lambda use: ->(elem) {yield elem}
-  #   expr = block_given? ? ->(elem) { yield elem } : ->(elem) { pattern === elem }
-  #   my_each { |elem| return false unless expr.call(elem) }
-  #   true
-  # end
-
-  # Below methods will only work with arrays but will not work if the array contains
-  # nil value
-  # def my_every?(argv=nil, &block)
-  #   # block = Proc.new { |item| item unless item.nil? || !item } unless block_given?
-  #   block = Proc.new { |item| item if argv === item} unless argv.nil?
-  #   self.my_each { |item| return false unless block.call(item) }
-    
-  #   true
-  # end
 end
 
 module Enumerable
@@ -87,46 +70,63 @@ module Enumerable
 
     true
   end
+
+
+  def my_count(&block)
+    return self.size unless block_given?
+
+    count = 0
+    self.my_each { |el| count += 1 if block.call(el) }
+    count
+  end
+
+  def my_map(&block)
+    return to_enum(:my_map) unless block_given?
+
+    array = Array.new(0)
+
+    case self
+    when Array
+      self.my_each { |el| array << block.call(el) }
+    when Hash
+      self.my_each { |k, v| array << block.call(k, v) }
+    end
+
+    array
+  end
+
+  # experiment more with inject.
+  def my_inject(accumulator = nil, &block)
+    arg = args[0] unless args.nil?
+
+    if arg.nil?
+      accumulator = self.first
+    else
+      accumulator = arg
+    end
+
+    self.my_each do |el|
+      accumulator = block.call(accumulator, el)
+    end
+
+    accumulator
+  end
+
+
 end
 
 class Array
   include Enumerable
 end
 
-arr = [1, 2, 3, 4]
-
-val = 4
-my_lamba = ->(el) { el == val }
-my_proc = Proc.new { |el| el == val }
-# my_lambda = lambda { el == val }
-# my_proc = proc { |el| el == val }
-
-
-
-
-# p arr.none?(String)
-# p arr.my_none?(String)
-# p arr.my_any?(Symbol)
-# p arr.any?
-
-# p arr.my_every? { |item| item == nil }
+# arr = %w(turtle rabbit)
 # hash = { a: 'apple', b: 'banana' }
 
-# arr.my_all? { |item| p item }
-# p arr.my_tot? { |item| item > 1 }
+arr = [2, 3]
+aproc = Proc.new { |sum, n| sum * n }
 
-# my_select (select is like filter)
-# p arr.my_select { |el| el > 11 }
-# p arr.select
-# p hash.my_select { |k,v| v.length > 5 }
+# arr.my_inject(&aproc)
+p arr.inject(&aproc)
 
-
-# my_each_with_index
-# arr.my_each_with_index { |el, index| p el, index }
-# hash.my_each_with_index { |k,v,i| p k, v, i }
-
-# my_each
-# arr.my_each { |el| p el }
-# p hash.my_each { |k, v| k }
 
 
